b5e4b651472c0ea11f57ab83ada15a21cff0feca
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2025-05-04 20:59:59 +0200

    format + fixed placement ememies

--- player/player_targeting_system/player_targeting_system.gd
+++ player/player_targeting_system/player_targeting_system.gd
@@ -14,26 +14,26 @@ class_name PlayerTargetingSystem
 ## Retargeting to left or right list targets can happen by motioning with
 ## the mouse or joystick in those directions.
 
-@export var signaling_node : Node
-@export var targeting_toggle_signal :String = "targeting_changed"
+@export var signaling_node: Node
+@export var targeting_toggle_signal: String = "targeting_changed"
 
-@export_range(1,10) var joystick_retarget_sensitivity :float = 5
-@export_range(1,10) var mouse_retarget_sensitivity :float = 3
+@export_range(1, 10) var joystick_retarget_sensitivity: float = 5
+@export_range(1, 10) var mouse_retarget_sensitivity: float = 3
 
-@onready var left_eye : Area3D= $LeftEye
-@onready var right_eye : Area3D = $RightEye
-@onready var center_eye : Area3D = $CenterEye
+@onready var left_eye: Area3D = $LeftEye
+@onready var right_eye: Area3D = $RightEye
+@onready var center_eye: Area3D = $CenterEye
 @onready var eyeline = $Eyeline
 
-var target_list : Array = []
-@onready var targeting = false 
+var target_list: Array = []
+@onready var targeting = false
 
 signal target_found
 
 @onready var reticle_control = $ReticleControl
 
 ## The group name of targetable objects if using groups
-@export var target_group_name : String = "targets"
+@export var target_group_name: String = "targets"
 ## The layers the eyes will scan when detecting targets
 @export_flags_3d_physics var target_detection_layer_mask = 3
 
@@ -43,25 +43,25 @@ func _ready():
 	right_eye.collision_mask = target_detection_layer_mask
 	eyeline.collision_mask = target_detection_layer_mask
 	
-	center_eye.connect("target_list_updated",_update_lists)
-	left_eye.connect("target_list_updated",_update_lists)
-	right_eye.connect("target_list_updated",_update_lists)
+	center_eye.connect("target_list_updated", _update_lists)
+	left_eye.connect("target_list_updated", _update_lists)
+	right_eye.connect("target_list_updated", _update_lists)
 	
 	if signaling_node:
 		if signaling_node.has_signal(targeting_toggle_signal):
-			signaling_node.connect(targeting_toggle_signal,_on_targeting_toggled)
+			signaling_node.connect(targeting_toggle_signal, _on_targeting_toggled)
 			
 func _update_lists():
 	target_list = center_eye.target_list
 	target_list += right_eye.target_list
 	target_list += left_eye.target_list
 
-func _input(_event:InputEvent): 
+func _input(_event: InputEvent):
 	# Senses input left or right on mouse or joypad
 	# and calls to change target to the left or right.
 	if targeting:
 		if _event is InputEventMouseMotion:
-			if abs(_event.relative.x) > mouse_retarget_sensitivity * 100: 
+			if abs(_event.relative.x) > mouse_retarget_sensitivity * 100:
 				var target_dir = sign(_event.relative.x)
 				select_new_target(target_dir, .6)
 
@@ -76,14 +76,13 @@ func _input(_event:InputEvent):
 		
 	
 func _on_targeting_toggled(_toggle):
-	#print("targeting toggled: " + str(_toggle))
 	targeting = _toggle
 	if targeting == false:
 		target_list.clear()
 	else:
 		get_target()
 	
-func select_new_target(_new_direction = -1,_delay = .5):
+func select_new_target(_new_direction = -1, _delay = .5):
 	if _new_direction == -1:
 		get_target(left_eye.target_list)
 	elif _new_direction == 1:
@@ -97,7 +96,7 @@ func _filter_body(body):
 		
 func eyeline_check(_new_target):
 	if _new_target:
-		eyeline.look_at(_new_target.global_position + Vector3.UP,Vector3.UP,false)
+		eyeline.look_at(_new_target.global_position + Vector3.UP, Vector3.UP, false)
 		await get_tree().process_frame
 		if eyeline.is_colliding():
 			if eyeline.get_collider() == _new_target:
@@ -110,5 +109,3 @@ func get_target(_list = target_list):
 		if await eyeline_check(new_target):
 			target_found.emit(new_target)
 			break
-	
-	
